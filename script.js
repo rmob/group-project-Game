483f68f2f5c848e3bf1c2af18099c3a394394566
Lance_Lake <temp@MostDeadlyGame.com>
2022-12-01 18:40:22 -0800

    Fixed scores and first submittal

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ var userNameEl = document.getElementById("userName");
 var userFormEl = document.getElementById("scoreBoard");
 
 
-var userId = localStorage.getItem("userId") || '';
 var score = 0;
+
+var userId = localStorage.getItem("userId") || '';
 
 var GetLeaderboard = 'http://www.5thwallgaming.com/Bootcamp/index.cfm?action=GetLeaderboard';
 apiSend('GetLeaderboard',GetLeaderboard);
@@ -142,7 +143,7 @@ function checkScore()
 		buttonEL.textContent = score;
 		localStorage.setItem("score", score);
 
-		if(score == 25 && timer1Active == 0)
+		if(score == 10 && timer1Active == 0)
 			{
 			button1 = document.createElement('button');
 			button1.innerHTML = '1 click per second';
@@ -153,7 +154,7 @@ function checkScore()
 			timer1Active = 1;
 			}
 
-		if(score == 75 && timer2Active == 0)
+		if(score == 25 && timer2Active == 0)
 			{
 			button2 = document.createElement('button');
 			button2.innerHTML = '5 clicks per second';
@@ -164,7 +165,7 @@ function checkScore()
 			timer2Active = 1;
 			}
 
-		if(score == 150 && timer3Active == 0)
+		if(score == 100 && timer3Active == 0)
 			{
 			button3 = document.createElement('button');
 			button3.innerHTML = '10 clicks per second';
@@ -175,7 +176,7 @@ function checkScore()
 			timer3Active = 1;
 			}
 
-		if(score == 250 && timer4Active == 0)
+		if(score == 200 && timer4Active == 0)
 			{
 			button4 = document.createElement('button');
 			button4.innerHTML = '50 clicks per second';
@@ -186,7 +187,7 @@ function checkScore()
 			timer4Active = 1;
 			}
 
-		if(score == 450 && timer5Active == 0)
+		if(score == 400 && timer5Active == 0)
 			{
 			button5 = document.createElement('button');
 			button5.innerHTML = '100 clicks per second';
@@ -197,7 +198,7 @@ function checkScore()
 			timer5Active = 1;
 			}
 
-		if(score == 750 && timer6Active == 0)
+		if(score == 700 && timer6Active == 0)
 			{
 			button6 = document.createElement('button');
 			button6.innerHTML = '500 clicks per second';
@@ -208,7 +209,7 @@ function checkScore()
 			timer6Active = 1;
 			}
 
-		if(score == 1250 && timer7Active == 0)
+		if(score == 1200 && timer7Active == 0)
 			{
 			button7 = document.createElement('button');
 			button7.innerHTML = '1,000 clicks per second';
@@ -219,7 +220,7 @@ function checkScore()
 			timer7Active = 1;
 			}
 
-		if(score == 2050 && timer8Active == 0)
+		if(score == 2000 && timer8Active == 0)
 			{
 			button8 = document.createElement('button');
 			button8.innerHTML = '5,000 clicks per second';
@@ -230,7 +231,7 @@ function checkScore()
 			timer8Active = 1;
 			}
 
-		if(score == 3350 && timer9Active == 0)
+		if(score == 3300 && timer9Active == 0)
 			{
 			button9 = document.createElement('button');
 			button9.innerHTML = '10,000 clicks per second';
@@ -241,7 +242,7 @@ function checkScore()
 			timer9Active = 1;
 			}
 
-		if(score == 5150 && timer10Active == 0)
+		if(score == 5100 && timer10Active == 0)
 			{
 			button10 = document.createElement('button');
 			button10.innerHTML = '50,000 clicks per second';
@@ -252,7 +253,7 @@ function checkScore()
 			timer10Active = 1;
 			}
 
-		if(score == 8550 && timer11Active == 0)
+		if(score == 8500 && timer11Active == 0)
 			{
 			button11 = document.createElement('button');
 			button11.innerHTML = '100,000 clicks per second';
@@ -318,6 +319,7 @@ resetEL.addEventListener	(
 submitEl.addEventListener	(
 	'click', function()
 	{
+		userId = localStorage.getItem("userId") || '';
 		var EnterScore = 'http://www.5thwallgaming.com/Bootcamp/index.cfm?action=EnterScore&score=' + score + '&userID=' + userId + '&Username=' + userNameEl.value;
 		apiSend("EnterScore", EnterScore);
 		apiSend('GetLeaderboard',GetLeaderboard);
